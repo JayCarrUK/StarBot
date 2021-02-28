8f142401bf7991bdd5a54263853b28097f020607
RaymonDev <40960636+RaymonDev@users.noreply.github.com>
2021-02-28 00:40:44 -0700

    Update star_bot.py
    just added some comments to make the code more clear

--- star_bot.py
+++ star_bot.py
@@ -1,25 +1,27 @@
+
+#imports libraries
 from decouple import config
 import requests
 from telegram.ext import Updater, MessageHandler, Filters
 
-token = config('TOKEN')
-request_url = config('REQUEST_URL')
+token = config('TOKEN') #gets telegram token
+request_url = config('REQUEST_URL') #gets request url from github API
 
 updater = Updater(token=token)
-dispatcher = updater.dispatcher
+dispatcher = updater.dispatcher #delivers updates to dispatcher
 
 
 def handler(bot, update):
     api = requests.get(url=request_url)
-    response = api.json()
+    response = api.json() #gets the json response from the API
     request = update.message.text
     result = ""
     for data in response:
         if data['name'] != request:
             continue
-        result = "Repository '%s' has %d Stars." % (request, data['stargazers_count'])
+        result = "Repository '%s' has %d Stars." % (request, data['stargazers_count']) #shows the stars of the repositories
         break
-    if result == "":
+    if result == "": #if the repository is not found, prints an error message
         result = "Repository not found."
     update.message.reply_text(result)
 
